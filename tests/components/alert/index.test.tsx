5b8f8da02d7a990eb67a182be5587bc54cc311eb
bytemebaby <hello@sameera.dev>
2024-01-03 18:03:34 +0530

    test: add few tests to check if intents are getting changed properly

--- tests/components/alert/index.test.tsx
+++ tests/components/alert/index.test.tsx
@@ -3,18 +3,21 @@ import Alert from "../../../src/lib/components/alert";
 
 describe("<Alert />", () => {
   it("should render correctly", () => {
-    render(<Alert>text</Alert>);
+    const { rerender } = render(<Alert >text</Alert>);
+    const component = screen.getByText("text");
+    expect(component).toBeInTheDocument();
+    expect(component).toHaveClass("border-bm_primary-300 bg-bm_primary-100 text-bm_primary-800");
 
-    const alert = screen.getByText("text");
-    expect(alert).toBeInTheDocument()
-  })
+    rerender(<Alert intent="secondary">text</Alert>);
+    expect(component).toHaveClass("border-bm_secondary-300 bg-bm_secondary-100 text-bm_secondary-800");
+  });
 
   it("should be dismissed after 3 seconds", () => {
     render(<Alert autoDissmissTime={3000}>text</Alert>);
     const alert = screen.getByText("text");
 
     waitFor(() => {
-      expect(alert).not.toBeInTheDocument()
+      expect(alert).not.toBeInTheDocument();
     });
-  })
+  });
 }); 
